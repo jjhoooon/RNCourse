79031685e36eeeee38664292ad2a4832f1ad20dc
장 재 훈 <peng2412@naver.com>
2024-02-23 17:45:21 +0900

    06-adding-linear-gradient

--- my-app/App.js
+++ my-app/App.js
@@ -1,11 +1,20 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient'
+
 import StartGameScreens from './screens/StartGameScreens';
+
 
 export default function App() {
   return (
-    <StartGameScreens />
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+      <StartGameScreens />
+    </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  }
+});
 
