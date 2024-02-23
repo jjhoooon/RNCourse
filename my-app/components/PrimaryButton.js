164513ffeaa7da62546d3453fdeb687f0ef9ebb3
장 재 훈 <peng2412@naver.com>
2024-02-23 17:32:40 +0900

    04-improving-buttons

--- my-app/components/PrimaryButton.js
+++ my-app/components/PrimaryButton.js
@@ -1,14 +1,48 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Pressable } from 'react-native';
 
 const PrimaryButton = ({ children }) => {
+
+    function pressHandler() {
+        console.log('Pressed!');
+    }
+
     return (
-        <View>
-            <Text>{children}</Text>
+        <View style={styles.buttonOuterContainer}>
+            <Pressable
+                style={({ pressed }) =>
+                    pressed
+                        ? [styles.buttonInnerContainer, styles.pressed]
+                        : styles.buttonInnerContainer
+                }
+                onPress={pressHandler}
+                android_ripple={{ color: '#640223' }}
+            >
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
         </View>
     );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: 'hidden',
+    },
+    buttonInnerContainer: {
+        backgroundColor: '#72063c',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2,
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center'
+    },
+    pressed: {
+        opacity: 0.75
+    }
+})
 
 export default PrimaryButton;
--- my-app/screens/StartGameScreens.js
+++ my-app/screens/StartGameScreens.js
@@ -5,7 +5,12 @@ import PrimaryButton from '../components/PrimaryButton';
 const StartGameScreens = () => {
     return (
         <View style={styles.inputContainer}>
-            <TextInput style={styles.numberInput} maxLength={2} />
+            <TextInput style={styles.numberInput}
+                maxLength={2}
+                keyboardType='number-pad'
+                autoCapitalize='none'
+                autoCorrect={false}
+            />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
