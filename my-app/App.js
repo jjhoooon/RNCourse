9380bdef30846ea768cf9f01afc64fc332b63bc8
장 재 훈 <peng2412@naver.com>
2024-02-23 18:00:14 +0900

    adding-background-image

--- my-app/App.js
+++ my-app/App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { ImageBackground, StyleSheet, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient'
 
 import StartGameScreens from './screens/StartGameScreens';
@@ -7,7 +7,14 @@ import StartGameScreens from './screens/StartGameScreens';
 export default function App() {
   return (
     <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
-      <StartGameScreens />
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        resizeMode='cover'
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreens />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -15,6 +22,9 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15
   }
 });
 
