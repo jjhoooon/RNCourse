9ff0b519d03d1fc5a31412497152126559ae2199
장 재 훈 <peng2412@naver.com>
2024-03-05 14:53:13 +0900

    getting-started-with-react-navigation

--- my-second-app/App.js
+++ my-second-app/App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
 import CategoriesScreen from './screens/CategoriesScreen';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
