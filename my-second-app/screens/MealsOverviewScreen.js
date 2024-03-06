10ec8d79350264789468cef8c660fb6b2a8cb524
장 재 훈 <peng2412@naver.com>
2024-03-06 15:19:10 +0900

    07-styling-navigation-options-dynamically(navigation props : study more

--- my-second-app/screens/MealsOverviewScreen.js
+++ my-second-app/screens/MealsOverviewScreen.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { View, StyleSheet, FlatList, Platform } from 'react-native';
 
-import { MEALS } from '../data/dummy-data';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 import MealItem from '../components/MealItem';
 
-const MealsOverviewScreen = ({ route }) => { // navigator의 Screen은 {navigation, route} 를 props로 제공하는데 각각 어떤 정보를 담고있는가?
+const MealsOverviewScreen = ({ route, navigation }) => { // navigator의 Screen은 {navigation, route} 를 props로 제공하는데 각각 어떤 정보를 담고있는가?
     const catId = route.params.categoryId
 
     const displayedMeals = MEALS.filter((mealItem) => {
         return mealItem.categoryIds.indexOf(catId) >= 0
     })
+
+    useLayoutEffect(() => {
+        const categoryTitle = CATEGORIES.find(
+            (category) => category.id === catId
+        ).title
+
+        navigation.setOptions({
+            title: categoryTitle,
+        })
+    }, [catId, navigation])
 
     function renderMealItem(itemData) {
         const item = itemData.item
