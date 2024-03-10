a1803d362cd009f6a3733f9e3e10fff86dcda8c0
장 재 훈 <peng2412@naver.com>
2024-03-11 00:44:05 +0900

    Drawer Navigation, reanimated/plugin Error(it cannot be fixed anymore)

---
+++ my-second-app/screens/FavoritesScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const FavoritesScreen = () => {
+    return (
+        <View>
+            <Text>The FavoritesScreen Here!</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default FavoritesScreen;
