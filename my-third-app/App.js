dbe5e41145d9a98b2c3be7da81b78ea3c5dac7fa
장 재 훈 <peng2412@naver.com>
2024-03-15 01:01:01 +0900

    03-adding-global-colors

--- my-third-app/App.js
+++ my-third-app/App.js
@@ -3,25 +3,70 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Foundation } from '@expo/vector-icons';
+
+
 import AllExpenses from './screens/AllExpenses';
 import RecentExpenses from './screens/RecentExpenses';
-// import ManageExpense from './screens/ManageExpense';
+import ManageExpense from './screens/ManageExpense';
 
+import { GlobalStyles } from './constants/styles';
 
 
 const Stack = createNativeStackNavigator()
 const Tab = createBottomTabNavigator();
+
+function ExpensesOverview() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        headerTintColor: GlobalStyles.colors.accent500,
+        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+      }}
+    >
+      <Tab.Screen
+        name="AllExpenses"
+        component={AllExpenses}
+        options={{
+          title: '<All Expenses>',
+          tabBarLabel: 'All',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="wallet" size={24} color={GlobalStyles.colors.accent500} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="RecentExpenses"
+        component={RecentExpenses}
+        options={{
+          title: '<Recent Expenses>',
+          tabBarLabel: 'Recent',
+          tabBarIcon: ({ color, size }) => (
+            <Foundation name="list" size={24} color={GlobalStyles.colors.accent500} />
+          )
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
+
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="AllExpenses" component={AllExpenses} />
-          <Tab.Screen name="RecentExpenses" component={RecentExpenses} />
-        </Tab.Navigator>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='ExpensesOverview'
+            component={ExpensesOverview}
+            options={{ headerShown: false }} />
+          <Stack.Screen name='ManageExpense' component={ManageExpense} />
+        </Stack.Navigator>
       </NavigationContainer>
     </>
   );
